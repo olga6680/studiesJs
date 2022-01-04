e8995a237c44b9ce2d5d69af791eb7c1bd3ef29e
Olga <boa906680@gmail.com>
2022-01-04 16:36:43 +0200

    Создание переменных с ответами пользователей

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 "use strict";
 
-//alert('HELLO');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//const result = confirm('Are you here?');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const answer = prompt('Вам есть 18?', '18');
-console.log(answer);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('На сколько оцените его?', ''),
+    d = prompt('Один из последних просмотренных фильмов?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
