68911e557071ec6ea9bc3434a6b99a93016b1a17
Olga <boa906680@gmail.com>
2022-01-06 10:59:04 +0200

    Поверхностные копии объектов и массивов

--- js/abstract.js
+++ js/abstract.js
@@ -224,3 +224,51 @@ console.log(arr);
 function compareNum(a, b) {
     return a - b;
 }
+
+//Копии объектов
+
+/* let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+  a: 5,
+  b: 1
+};
+
+/* const copy = obj; //ССылка
+
+copy.a = 10;
+console.log(copy);
+console.log(obj); */
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+
+console.log(newNumbers);
+console.log(numbers);
