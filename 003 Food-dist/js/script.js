ec888b905097cb2b57713ea19209275f01c72f8d
Olga <boa906680@gmail.com>
2022-01-30 21:25:06 +0200

    Оператор rest
    Указывкает на то, что в дальнейшем может быть сколько угодно операторов, мы не знаем каких.

--- 003 Food-dist/js/script.js
+++ 003 Food-dist/js/script.js
@@ -192,12 +192,13 @@ window.addEventListener('DOMContentLoaded', () => {
     //Используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector); //сюда будут помещаться карточки. Сейчас это container. Но он может быть любым другим
             this.transfer = 27;
             this.chengeToUAH(); //можно вызывать метод прямо в конструкторе
@@ -209,8 +210,29 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() { //render() -классическое название метода, чтобы сформировать верстку
             const element = document.createElement('div');
+            if (this.classes.length === 0) { //если не был задан ни один класс, то задаем всегда menu__item
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className)); //присваем класс menu__item div. Избавляемся от лишнего div
+            }
+
+            /*             element.innerHTML = `
+                            <div class="menu__item">
+                                <img src=${this.src} alt=${this.alt}>
+                                <h3 class="menu__item-subtitle">${this.title}</h3>
+                                <div class="menu__item-descr">${this.descr}</div>
+                                <div class="menu__item-divider"></div>
+                                <div class="menu__item-price">
+                                    <div class="menu__item-cost">Цена:</div>
+                                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                                </div>
+                            </div>
+                        `;
+                        this.parent.append(element); */
+
+            this.classes.forEach(className => element.classList.add(className)); //присваем класс menu__item div. Избавляемся от лишнего div
             element.innerHTML = `
-                <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -219,7 +241,6 @@ window.addEventListener('DOMContentLoaded', () => {
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>
             `;
             this.parent.append(element);
         }
@@ -233,7 +254,9 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9, //9$
-        '.menu .container'
+        '.menu .container',
+        //'menu__item',  //теперь мы можем добавлять сколько угодно классов
+        //'big'
     ).render();
 
     new MenuCard(
@@ -242,7 +265,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         14,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
     ).render();
 
     new MenuCard(
@@ -251,6 +275,7 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         21,
-        '.menu .container'
+        '.menu .container',
+        //'menu__item',
     ).render();
 });
